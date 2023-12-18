d510dd21377bd45ab5f3be44e27fd2e5dc248e35
MMK <staph103@gmail.com>
2023-12-18 16:27:37 -0500

    working on structure of the pages

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const shapes = require('./lib/shapes')
+const inquirer = require('inquirer');
+
+
+inquirer
+.prompt([
+    // Pass your questions in here 
+    { type: 'maxlength-input', name: 'title', message: 'What is the name of your logo (must be 3 characters)', maxLength: 3},
+    { type: 'input', name: 'color', message: 'What color would you like for the text' },
+    { type: 'list', name: 'shape', message: 'Pick a shape', choices: ['circle', 'triangle', 'square']},
+    { type: 'input', name: 'color', message: 'What color would you like for the shape' },
+
+])
+.then((answers) => {
+
+    const logoInfo = 
+    fs.writeFile('logo.svg',shapes(answers), (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("Successfully generated")
+        }
+    })
+
+})
+
+// // .then((answers) => {
+//     const htmlPageContent = generateHTML(answers);
+
+//     fs.writeFile('index.html', htmlPageContent, (err) =>
+//       err ? console.log(err) : console.log('Successfully created index.html!')
+//     );
+//   });
