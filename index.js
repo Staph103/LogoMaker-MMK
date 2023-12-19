c66bb677d3f802dc74c43a7c6a36d56bf13d87d0
MMK <staph103@gmail.com>
2023-12-19 14:45:20 -0500

    fixed the error in first prompt to be 3 characters

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 const fs = require('fs')
-const shapes = require('./lib/shapes')
+// const shapes = require('./logoGenerator.test')
 const inquirer = require('inquirer');
 
 
-inquirer
-.prompt([
-    // Pass your questions in here 
-    { type: 'maxlength-input', name: 'title', message: 'What is the name of your logo (must be 3 characters)',validate: (input) => input.length <= 3},
-    { type: 'input', name: 'color', message: 'What color would you like for the text' },
-    { type: 'list', name: 'shape', message: 'Pick a shape', choices: ['circle', 'triangle', 'square']},
-    { type: 'input', name: 'color', message: 'What color would you like for the shape' },
+function logoGenerator(){
+    inquirer.prompt([
+        // Pass your questions in here 
+        { type: 'maxlength-input', name: 'letters', message: 'What is the name of your logo (must be 3 characters)', validate: (input) => input.length <= 3 && input.length >= 3},
+        { type: 'input', name: 'txtColor', message: 'What color would you like for the text (color name or hexadecimal)' },
+        { type: 'list', name: 'shape', message: 'Pick a shape', choices: ['circle', 'triangle', 'square']},
+        { type: 'input', name: 'shapeColor', message: 'What color would you like for the shape (color name or hexadecimal)' },
 
-])
-.then((answers) => {
-
-    const logoInfo = 
-    fs.writeFile('logo.svg',shapes(answers), (err) => {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            console.log("Successfully generated")
-        }
+    ]).then((response) => {
+        const svgLogo = `
+        <svg width ='300' height='200'>
+        <${response.shape} fill ="${response.shapeColor}"/>
+        <text x="20" y="30"  fill="${response.txtColor}">${response.letters}/>
+        </svg>  
+        `;
+        fs.writeFile('logo.svg',svgLogo, (err) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Successfully generated")
+            }
+        })
     })
+}
 
-})
+    
 
-// // .then((answers) => {
-//     const htmlPageContent = generateHTML(answers);
 
-//     fs.writeFile('index.html', htmlPageContent, (err) =>
-//       err ? console.log(err) : console.log('Successfully created index.html!')
-//     );
-//   });
+
+    // .then((answers) => {
+
+    //     const logoInfo = 
+        
+
+//     })
+// }
+    
+logoGenerator();
