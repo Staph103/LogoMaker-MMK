84520c520c575a1fa91b84180bc786a384c8700f
MMK <staph103@gmail.com>
2023-12-20 21:04:24 -0500

    Worked on shapes test, the last teis failing and I cannot figure it out

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const fs = require('fs')
 const inquirer = require('inquirer');
 const { shapes, Sqr, Tri, Cir } = require("./lib/shapes")
 
+//function that ask user for input 
 function logoGenerator() {
     inquirer.prompt([
-        // Pass your questions in here 
+
         {
             type: 'input',
             name: 'letters',
             message: 'What is the name of your logo (must be 3 characters)',
+            //forces user to enter 3 characters
             validate: (input) => input.length <= 3 && input.length >= 3
         },
-        { type: 'input', name: 'txtColor', message: 'What color would you like for the text (color name or hexadecimal)' },
-        { type: 'list', name: 'shape', message: 'Pick a shape', choices: ['circle', 'triangle', 'square'] },
-        { type: 'input', name: 'shapeColor', message: 'What color would you like for the shape (color name or hexadecimal)' },
-        
+
+        {
+            type: 'input',
+            name: 'txtColor',
+            message: 'What color would you like for the text (color name or hexadecimal)'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Pick a shape',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color would you like for the shape (color name or hexadecimal)'
+        },
+
     ]).then((res) => {
         let newShape;
-
+        //
         switch (res.shape) {
             case 'circle':
                 newShape = new Cir(res.letters, res.txtColor, res.shapeColor);
@@ -32,6 +48,8 @@ function logoGenerator() {
                 break;
 
         }
+
+        //creates the svg file based on data retrieved from user input
 
         fs.writeFile('logo.svg', newShape.render(), (err) => {
             if (err) {
